1b408549710d3148b9bcc7453f682660efa07a03
biennd279 <bien.uet279@gmail.com>
2022-08-10 22:31:57 +0700

    fix some misconfig

--- docs/.vuepress/theme/client/layouts/Layout.vue
+++ docs/.vuepress/theme/client/layouts/Layout.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script lang="ts" setup>
-import ParentLayout from "@vuepress/theme-default/lib/client/layouts/Layout"
+import ParentLayout from "@vuepress/theme-default/lib/client/layouts/Layout.vue"
 </script>
 
 <style scoped>
